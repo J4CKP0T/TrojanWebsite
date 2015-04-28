b3cf58b04b74aeddcfa1a01d07cbf082cd763d1a
Danny Christanto <dnnych@gmail.com>
2015-04-27 18:25:09 -0700

    make brochure downloadable

--- server/routes.js
+++ server/routes.js
@@ -15,7 +15,10 @@ module.exports = function(app) {
   app.use('/auth', require('./auth'));
   
   // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+  app.route('/:url(api|auth|components|app|bower_components)/*')
+   .get(errors[404]);
+
+  app.route('/assets/images/*')
    .get(errors[404]);
 
   // All other routes should redirect to the index.html
