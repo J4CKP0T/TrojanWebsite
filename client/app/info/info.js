6c63734bd14e8ef5dce7f7092b87004edebf6e50
Danny Christanto <dnnych@gmail.com>
2015-04-27 18:31:10 -0700

    remove info route in client

--- client/app/info/info.js
+++ client/app/info/info.js
@@ -1,63 +1,63 @@
-'use strict';
+// 'use strict';
 
-angular.module('trojanApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('info', {
-      	abstract: true,
-        url: '/info',
-        templateUrl: 'app/info/info.html'
-      })
-      .state('info.index', {
-        url: '',
-        templateUrl: 'app/info/index/index.html'      
-      })
-      .state('info.articles', {
-      	url: '/articles',
-      	views: {
-          '': {
-            templateUrl: 'app/info/articles/articles.html'
-          }
-        }
-      })
-      .state('info.news', {
-      	url: '/news',
-      	views: {
-          '': {
-            templateUrl: 'app/info/news/news.html'
-          }
-        }
-      })
-      .state('info.prices', {
-      	url: '/prices',
-      	views: {
-          '': {
-            templateUrl: 'app/info/prices/prices.html'
-          }
-        }
-      })
-      .state('info.procedures', {
-        url: '/procedures',
-        views: {
-          '': {
-            templateUrl: 'app/info/procedures/procedures.html'
-          }
-        }
-      })
-      .state('info.shipments', {
-        url: '/shipments',
-        views: {
-          '': {
-            templateUrl: 'app/info/shipments/shipments.html'
-          }
-        }
-      })
-      .state('info.statistics', {
-        url: '/statistics',
-        views: {
-          '': {
-            templateUrl: 'app/info/statistics/statistics.html'
-          }
-        }
-      });
-  });
+// angular.module('trojanApp')
+//   .config(function ($stateProvider) {
+//     $stateProvider
+//       .state('info', {
+//       	abstract: true,
+//         url: '/info',
+//         templateUrl: 'app/info/info.html'
+//       })
+//       .state('info.index', {
+//         url: '',
+//         templateUrl: 'app/info/index/index.html'      
+//       })
+//       .state('info.articles', {
+//       	url: '/articles',
+//       	views: {
+//           '': {
+//             templateUrl: 'app/info/articles/articles.html'
+//           }
+//         }
+//       })
+//       .state('info.news', {
+//       	url: '/news',
+//       	views: {
+//           '': {
+//             templateUrl: 'app/info/news/news.html'
+//           }
+//         }
+//       })
+//       .state('info.prices', {
+//       	url: '/prices',
+//       	views: {
+//           '': {
+//             templateUrl: 'app/info/prices/prices.html'
+//           }
+//         }
+//       })
+//       .state('info.procedures', {
+//         url: '/procedures',
+//         views: {
+//           '': {
+//             templateUrl: 'app/info/procedures/procedures.html'
+//           }
+//         }
+//       })
+//       .state('info.shipments', {
+//         url: '/shipments',
+//         views: {
+//           '': {
+//             templateUrl: 'app/info/shipments/shipments.html'
+//           }
+//         }
+//       })
+//       .state('info.statistics', {
+//         url: '/statistics',
+//         views: {
+//           '': {
+//             templateUrl: 'app/info/statistics/statistics.html'
+//           }
+//         }
+//       });
+//   });
